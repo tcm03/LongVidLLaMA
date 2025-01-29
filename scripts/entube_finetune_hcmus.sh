75f6982c427ae54b2f0b08fe8e745accee38fd1c
tcm03 <21125019@student.hcmus.edu.vn>
2025-01-29 14:34:25 +0000

    commit before merge with local

--- scripts/entube_finetune_hcmus.sh
+++ scripts/entube_finetune_hcmus.sh
@@ -5,7 +5,7 @@ PATH_TO_JSON_VAL="/media02/nthuy/data/EnTube_preprocessing/data/EnTube_15m_test.
 PATH_TO_FOLDER="/media02/nthuy/data/entube/EnTube"
 VERSION="qwen"
 
-CUDA_LAUNCH_BLOCKING=1 TORCH_DISTRIBUTED_DEBUG=DETAIL torchrun --nproc_per_node=2 --nnodes=1 \
+CUDA_LAUNCH_BLOCKING=1 TORCH_DISTRIBUTED_DEBUG=DETAIL torchrun --nproc_per_node=4 --nnodes=1 \
     longvu/finetune.py \
     --output_dir "/tmp/longvu/" \
     --input_model_filename $PREV_STAGE_CHECKPOINT \
