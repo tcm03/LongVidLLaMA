f9820f811336a8dd0606d3f55685070b6b926b5d
tcm03 <21125019@student.hcmus.edu.vn>
2025-01-31 00:12:23 +0700

    Add custom compute_loss to LLaVATrainer

--- longvu/mm_trainer.py
+++ longvu/mm_trainer.py
@@ -10,7 +10,9 @@ from torch.utils.data import DataLoader, Sampler
 
 from transformers import Trainer
 
-from transformers.trainer import ALL_LAYERNORM_LAYERS, get_parameter_names, has_length
+from transformers.trainer import ALL_LAYERNORM_LAYERS, get_parameter_names, has_length, _is_peft_model
+from transformers.models.auto.modeling_auto import MODEL_FOR_CAUSAL_LM_MAPPING_NAMES
+import logging
 
 
 # pyre-fixme[3]: Return type must be annotated.
@@ -492,3 +494,41 @@ class LLaVATrainer(Trainer):
         else:
             # pyre-fixme[16]: `Trainer` has no attribute `_save`.
             super(LLaVATrainer, self)._save(output_dir, state_dict)
+
+    def compute_loss(self, model, inputs, return_outputs=False):
+        """
+        How the loss is computed by Trainer. By default, all models return the loss in the first element.
+
+        Subclass and override for custom behavior.
+        """
+        if self.label_smoother is not None and "labels" in inputs:
+            labels = inputs.pop("labels")
+        else:
+            labels = None
+        outputs = model(**inputs)
+        logging.info(f'outputs={outputs}')
+        # Save past state if it exists
+        # TODO: this needs to be fixed and made cleaner later.
+        if self.args.past_index >= 0:
+            self._past = outputs[self.args.past_index]
+
+        if labels is not None:
+            unwrapped_model = self.accelerator.unwrap_model(model)
+            if _is_peft_model(unwrapped_model):
+                model_name = unwrapped_model.base_model.model._get_name()
+            else:
+                model_name = unwrapped_model._get_name()
+            if model_name in MODEL_FOR_CAUSAL_LM_MAPPING_NAMES.values():
+                loss = self.label_smoother(outputs, labels, shift_labels=True)
+            else:
+                loss = self.label_smoother(outputs, labels)
+        else:
+            if isinstance(outputs, dict) and "loss" not in outputs:
+                raise ValueError(
+                    "The model did not return a loss from the inputs, only the following keys: "
+                    f"{','.join(outputs.keys())}. For reference, the inputs it received are {','.join(inputs.keys())}."
+                )
+            # We don't use .loss here since the model may return tuples instead of ModelOutput.
+            loss = outputs["loss"] if isinstance(outputs, dict) else outputs[0]
+
+        return (loss, outputs) if return_outputs else loss
