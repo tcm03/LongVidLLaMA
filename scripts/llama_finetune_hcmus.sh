9a90d4768ebc5c5ba2ff9db14df4a1da04584417
tcm03 <21125019@student.hcmus.edu.vn>
2025-02-09 17:33:46 +0000

    Update scripts back to fsdp

--- scripts/llama_finetune_hcmus.sh
+++ scripts/llama_finetune_hcmus.sh
@@ -1,8 +1,8 @@
 
 PREV_STAGE_CHECKPOINT="./checkpoints/longvu_llama3_2"
-PATH_TO_JSON_TRAIN="/media02/nthuy/data/EnTube_preprocessing/data/EnTube_1m_train.json"
-PATH_TO_JSON_VAL="/media02/nthuy/data/EnTube_preprocessing/data/EnTube_1m_test.json"
-PATH_TO_FOLDER="/media02/nthuy/data/entube/EnTube"
+PATH_TO_JSON_TRAIN="/media02/nthuy/data/EnTube_preprocessing/data/EnTube_2m_train.json"
+PATH_TO_JSON_VAL="/media02/nthuy/data/EnTube_preprocessing/data/EnTube_2m_test.json"
+PATH_TO_FOLDER="/media02/nthuy/data/entube/EnTube/data"
 VERSION="llama3"
 
 CUDA_LAUNCH_BLOCKING=1 TORCH_DISTRIBUTED_DEBUG=DETAIL torchrun --nproc_per_node=2 --rdzv_endpoint=localhost:29501 --nnodes=1 \
@@ -46,6 +46,8 @@ CUDA_LAUNCH_BLOCKING=1 TORCH_DISTRIBUTED_DEBUG=DETAIL torchrun --nproc_per_node=
     --tune_mm_mlp_adapter False \
     --freeze_mm_mlp_adapter False \
     --freeze_backbone True \
+    --fsdp "full_shard auto_wrap" \
+    --fsdp_transformer_layer_cls_to_wrap 'LlamaDecoderLayer' \
     --gradient_checkpointing True \
     --mm_projector_type sva \
     --image_token_len 144 \
