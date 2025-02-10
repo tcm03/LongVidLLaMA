d57f8043ceeb38b3011c122e08e399c252159107
tcm03 <21125019@student.hcmus.edu.vn>
2025-02-10 22:45:34 +0700

    Understand multimodal inputs: part 8

--- longvu/mm_trainer.py
+++ longvu/mm_trainer.py
@@ -521,7 +521,13 @@ class LLaVATrainer(Trainer):
         else:
             logits = outputs.logits
             loss_val = outputs.loss
+            debug_tensor("outputs.hidden_states", outputs.hidden_states)
+            tcm_logger.debug(f"outputs.hidden_states: {outputs.hidden_states}")
+            debug_tensor("outputs.attentions", outputs.attentions)
+            tcm_logger.debug(f"outputs.attentions: {outputs.attentions}")
+            
         output_ids = logits.argmax(dim=-1)
+        debug_tensor("output_ids", output_ids)
         assert len(output_ids) == len(inputs['input_ids']), 'Same batch size required'
         decoded_outputs = self.tokenizer.batch_decode(output_ids[..., :min(100, output_ids.shape[-1])], skip_special_tokens=True)
         tcm_logger.debug(f'loss={loss_val}')
