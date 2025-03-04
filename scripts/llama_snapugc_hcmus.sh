3068da2fc8379906e6d88ab0ce499bd2fe0c6ecc
tcm03 <21125019@student.hcmus.edu.vn>
2025-03-04 09:25:05 +0700

    batch_eval_metrics+include_inputs_for_metrics

--- scripts/llama_snapugc_hcmus.sh
+++ scripts/llama_snapugc_hcmus.sh
@@ -23,7 +23,6 @@ CUDA_LAUNCH_BLOCKING=1 TORCH_DISTRIBUTED_DEBUG=DETAIL torchrun --nproc_per_node=
     --per_device_train_batch_size 1 \
     --per_device_eval_batch_size 1 \
     --gradient_accumulation_steps 4 \
-    --batch_eval_metrics True \
     --save_steps 625 \
     --eval_steps 500 \
     --logging_steps 5 \
@@ -60,5 +59,6 @@ CUDA_LAUNCH_BLOCKING=1 TORCH_DISTRIBUTED_DEBUG=DETAIL torchrun --nproc_per_node=
     --drop_threshold 0.75 \
     --label_names labels \
     --include_inputs_for_metrics True \
+    --batch_eval_metrics True \
     --torch_empty_cache_steps 1 \
     --save_only_model True
