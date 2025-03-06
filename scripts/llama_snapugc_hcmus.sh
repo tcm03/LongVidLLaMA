2b7ed89b9c714a22cb77fa04b20d84a69f81b9ba
tcm03 <21125019@student.hcmus.edu.vn>
2025-03-06 16:06:03 +0000

    start commit on this branch back_fix_oom

--- scripts/llama_snapugc_hcmus.sh
+++ scripts/llama_snapugc_hcmus.sh
@@ -1,11 +1,11 @@
 
 PREV_STAGE_CHECKPOINT="./checkpoints/longvu_llama3_2"
-PATH_TO_JSON_TRAIN="/raid/nthuy/SnapUGC/snapugc_60s_4eval_train.json"
-PATH_TO_JSON_VAL="/raid/nthuy/SnapUGC/snapugc_60s_test.json"
+PATH_TO_JSON_TRAIN="/raid/nthuy/SnapUGC/snapugc_60s_train.json"
+PATH_TO_JSON_VAL="/raid/nthuy/SnapUGC/snapugc_mini_test.json"
 PATH_TO_FOLDER="/raid/nthuy/SnapUGC"
 VERSION="llama3"
 
-CUDA_LAUNCH_BLOCKING=1 TORCH_DISTRIBUTED_DEBUG=DETAIL torchrun --nproc_per_node=3 --rdzv_endpoint=localhost:29502 --nnodes=1 \
+CUDA_LAUNCH_BLOCKING=1 TORCH_DISTRIBUTED_DEBUG=DETAIL torchrun --nproc_per_node=4 --rdzv_endpoint=localhost:29502 --nnodes=1 \
     longvu/finetune_llama.py \
     --output_dir "/tmp/longvu/" \
     --input_model_filename $PREV_STAGE_CHECKPOINT \
